0a1e8c56dfe33bd235d79605146a6afff247f322
Altemush Bhatti <altemush@gmail.com>
2021-05-08 02:28:23 -0700

    feat: create DisplayPage with Header component
    Currently DisplayPage only contains the header

---
+++ my-reads/src/pages/DisplayPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DisplayPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+const Header = () => (
+  <div className="App-header">
+    <h1>MyReads</h1>
+  </div>
+);
+
+export default DisplayPage;
