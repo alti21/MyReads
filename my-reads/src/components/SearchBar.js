ed53fee3fddf7e6fa8d3c8b144b0f91e02286d67
Altemush Bhatti <altemush@gmail.com>
2021-05-16 15:07:47 -0700

    feat: style search bar

--- my-reads/src/components/SearchBar.js
+++ my-reads/src/components/SearchBar.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchBar = ({ type, placeholder }) => {
+const SearchBar = ({ type, placeholder, className, component }) => {
   return (
     <div>
-      <input type={type} placeholder={placeholder} />
+      {component}
+      <input type={type} placeholder={placeholder} className={className} />
     </div>
   );
 };
 
 SearchBar.propTypes = {
   type: PropTypes.string.isRequired,
+  className: PropTypes.string,
   placeholder: PropTypes.string,
+  component: PropTypes.elementType.isRequired,
 };
 
 SearchBar.defaultProps = {
   placeholder: 'Search',
+  className: 'search-bar',
 };
 
 export default SearchBar;
